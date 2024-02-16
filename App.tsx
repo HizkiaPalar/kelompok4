848fcb965b980c971bedf3fe09fb3e1ac403bb73
HizkiaPalar <Hizkiapalar294@gmail.com>
2024-02-16 10:19:35 +0800

    exercise 2

--- App.tsx
+++ App.tsx
@@ -1,103 +1,91 @@
-import React from 'react';
-import {ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import { View, 
+  Text, 
+  TextInput, 
+  Image, 
+  ScrollView, 
+  StyleSheet,
+} from 'react-native';
+import React from 'react'
 
 const App = () => {
-  const onPress = () => setCount(prevCount => prevCount + 1);
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.th}>Welcome</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.h}>
+         <Text style={styles.th}>Basic Components RN</Text>
       </View>
-      <ScrollView>
-        <View style={styles.u}>
-          <Text style={styles.username}> Username </Text>
-        </View>
+      <View style={styles.content}>
+        <Image
+          source={{ uri: 'https://designdrizzle.com/wp-content/uploads/Coder.png' }}
+          style={styles.image2}
+        />
+        <Text style={styles.contentText}>Ask me a question!</Text>
         <TextInput
-          placeholder='Masukkan username anda'
+          placeholder='Enter a question...'
           style={styles.input}
         />
-        <View style={styles.p}>
-          <Text style={styles.pass}> Password </Text>
-        </View>
-        <TextInput
-          placeholder='Masukkan password anda'
-          style={styles.input2}
+        <Image
+          source={require('./assets/gambar.jpg')}
+          style={styles.image}
         />
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-          <Text style={styles.s}>Sign In</Text>
-        </TouchableOpacity>
-      </ScrollView>
-    </View>
-  );
+      </View>
+      <View>
+        <Text style={styles.text}>Mobile App Development</Text>
+      </View>
+    </ScrollView>
+  );s
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
+    backgroundColor:'lightsalmon',
+    padding: 20,
+  },
+  h: {
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  th: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color:'black',
+  },
+  content: {
     flex: 1,
     justifyContent: 'center',
-    paddingHorizontal: 10,
-  },
-  header:{
-    marginBottom:50,
-    marginTop:25,
-    marginLeft:10,
-  },
-  th:{
-    fontSize:35,
-    fontWeight:'bold',
-    color:'black'
-  },
-  u:{
-    marginBottom:6,
-  },
-  username:{
-    fontWeight:'bold',
-    marginLeft:4,
-    fontSize:20,
-    color:'grey',
-  },
-  p:{
-    marginBottom:6,
-  },
-  pass:{
-    fontWeight:'bold',
-    fontSize:20,
-    marginLeft:4,
-    color:'grey',
-  },
-  input:{
-    height: 50,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius:10,
-    marginBottom: 16,
-    marginLeft:9,
-    paddingLeft: 10,
-    width: '95%', 
-  },
-  input2:{
-    height: 50,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius:10,
-    marginBottom: 16,
-    marginLeft:9,
-    paddingLeft: 10,
-    width: '95%',
-  },
-  button: {
     alignItems: 'center',
-    backgroundColor: 'darkorange',
-    padding: 10,
-    marginTop:30,
-    width:'95%',
-    height: 50,
-    borderRadius:15,
-    marginLeft:9,
   },
-  s:{
-    paddingTop:4,
-    color:'white',
+  contentText: {
+    fontSize: 18,
+    marginBottom: 16,
+    color:'black',
+  },
+  input: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius:6,
+    marginBottom: 16,
+    paddingLeft: 10,
+    width: '80%', 
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginTop: 16,
+  },
+  image2: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginTop: -65,
+    marginBottom: 20,
+  },
+  text:{
+    textAlign: 'center',
+    color:'black',
+    fontSize: 15,
   },
 });
 
